d1893a3a89c8cf935221d66fa8acf329d33a500d
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2024-03-17 12:54:28 -0400

    amads files

---
+++ QA-Bot/runScript.sh
@@ -0,0 +1,30 @@
+export HF_DATASETS_CACHE="/research/nfs_fosler_1/models/huggingface_cache"
+export CUDA_VISIBLE_DEVICES=1
+
+
+# Make sure LOGGER_NAME and --model_name are correct
+LOGGER_NAME="t5_xl_goldFacts"
+python3 lightning_t5_trainer.py \
+	--model_directory "/research/nfs_fosler_1/amadh/cosi/modelStore/" \
+	--model_path "/research/nfs_fosler_1/amadh/cosi/modelStore/${LOGGER_NAME}/" \
+	--train_data "data/llama_fine_tune_train_set.json" \
+	--dev_data "data/llama_fine_tune_dev_set.json" \
+	--test_data "data/llama_fine_tune_test_set.json" \
+	--evidence_data "data/evidence.tsv" \
+	--num_turns 3 \
+	--model_name "google/flan-t5-xl" \
+	--generation_input "data/llama_fine_tune_dev_set.json" \
+	--generation_output "data/results/${LOGGER_NAME}_dev_generations.tsv" \
+	--data_seed 42 \
+	--train_seed 42 \
+	--dev_ratio 0.1 \
+	--max_epochs 100 \
+	--lr 0.001 \
+	--batch_size 64 \
+	--max_tkn_length 2048 \
+	--num_workers 8 \
+	--patience 10 \
+	--logger_name $LOGGER_NAME \
+	--notes "" \
+	--gpu_num 0 \
+	--run_generation
