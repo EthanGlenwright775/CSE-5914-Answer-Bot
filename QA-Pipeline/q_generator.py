fde334c7216860cb3945b5446bf4ba9ffd5f4c7a
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2024-03-24 04:21:34 -0400

    q eval update

--- QA-Pipeline/q_generator.py
+++ QA-Pipeline/q_generator.py
@@ -1,4 +1,5 @@
 import torch
+import threading
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForSequenceClassification
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -8,7 +9,17 @@ model = model.to(device)
 tokenizer_eval = AutoTokenizer.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator")
 model_eval = AutoModelForSequenceClassification.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator")
 model_eval = model_eval.to(device)
-Q_EVAL_THRESHOLD = 1
+
+q_eval_threshold = 1
+q_count = 0
+rejected_count = 0
+
+q_count_lock = threading.Lock()
+rejected_count_lock = threading.Lock()
+
+def set_q_eval_threshold(threshold: int):
+    q_eval_threshold = threshold
+    print(f"Question evaluator threshold set to {q_eval_threshold}")
 
 def __generate__(answer: str, context: str) -> list[dict[str, str]]:
         concat = "<answer> " + answer + " <context> " + context
@@ -21,35 +32,20 @@ def __generate__(answer: str, context: str) -> list[dict[str, str]]:
         question = tokenizer.batch_decode(question_tokenized, skip_special_tokens=True)
         return question
 
-
-def generate_questions_multicontext(answer_list: list[str], context_list: list[str]) -> list[dict[str, str]]:
-
-    if not (isinstance(answer_list, list) and isinstance(context_list, list)): raise RuntimeError("Both Parameters must be lists to use multicontext.")
-    if (len(answer_list) != len(context_list)): raise RuntimeError("Answer list length must match Context list length to use multicontext.")
-
-    qa_pair_list = []
-
-    for index in range(len(answer_list)):
-        question = __generate__(answer_list[index], context_list[index])
-        question = question[0]
-        question = question[:question.find("?") + 1]
-        qa_pair_list.append({"question": question, "answer": answer_list[index]})
-
-    return qa_pair_list
-
-def eval_qa_pair(q, a, threshold):
-    encoded_input = tokenizer_eval(text=q, text_pair=a, truncation=True, return_tensors="pt")
-    output = model_eval(**encoded_input)
-    output = output[0][0][1].item()
-
-    if output > threshold:
-        return True
-    else:
-        # print("q: " + q)
-        # print("a: " + a)
-        # print("score: " + output)
-        return False
-
+#def generate_questions_multicontext(answer_list: list[str], context_list: list[str]) -> list[dict[str, str]]:
+#
+#    if not (isinstance(answer_list, list) and isinstance(context_list, list)): raise RuntimeError("Both Parameters must be lists to use multicontext.")
+#    if (len(answer_list) != len(context_list)): raise RuntimeError("Answer list length must match Context list length to use multicontext.")
+#
+#    qa_pair_list = []
+#
+#    for index in range(len(answer_list)):
+#        question = __generate__(answer_list[index], context_list[index])
+#        question = question[0]
+#        question = question[:question.find("?") + 1]
+#        qa_pair_list.append({"question": question, "answer": answer_list[index]})
+#
+#    return qa_pair_list
 
 def generate_questions_monocontext(answer_list: list[str], context: str) -> list[dict[str, str]]:
 
@@ -63,8 +59,42 @@ def generate_questions_monocontext(answer_list: list[str], context: str) -> list
         question = __generate__(answer, context)
         question = question[0]
         question = question[:question.find("?") + 1]
-        if eval_qa_pair(question,answer, Q_EVAL_THRESHOLD):
+        count_questions()
+        if eval_qa_pair(question,answer):
             qa_pair_list.append({"question": question, "answer": answer})
+        else:
+            count_rejected_questions()
 
     return qa_pair_list
 
+def eval_qa_pair(q: str, a: str):
+    global q_eval_threshold
+    encoded_input = tokenizer_eval(text=q, text_pair=a, truncation=True, return_tensors="pt").to(device)
+    output = model_eval(**encoded_input)
+    output = output[0][0][1].item()
+
+    if output > q_eval_threshold:
+        return True
+    else:
+        # print("q: " + q)
+        # print("a: " + a)
+        # print("score: " + output)
+        return False
+    
+def count_questions():
+    global q_count
+    with q_count_lock:
+        q_count += 1
+
+def count_rejected_questions():
+    global rejected_count
+    with rejected_count_lock:
+        rejected_count += 1
+
+def print_q_eval_stats():
+    global q_count
+    global rejected_count
+    print(f"Total # of questions: {q_count}")
+    print(f"Total # of rejected questions: {rejected_count}")
+    rejection_rate = (rejected_count / q_count) * 100
+    print(f"Question rejection rate: {rejection_rate} %")
