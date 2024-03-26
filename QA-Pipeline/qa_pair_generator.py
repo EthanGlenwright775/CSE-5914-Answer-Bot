9bc20fffc7591e98725ce6e9d2bd5914b85dc5b4
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2024-03-26 17:05:07 -0400

    fix chunking

--- QA-Pipeline/qa_pair_generator.py
+++ QA-Pipeline/qa_pair_generator.py
@@ -5,39 +5,41 @@ import re
 
 nltk.download('punkt', quiet=True)
 from nltk.tokenize import sent_tokenize
-
+CHUNK_SIZE = 200
 
 # Implement a sliding window technique for the context
 
-def chunk_text(text: str, window_size: int, overlap: int = 10) -> list[str]:
-    words = text.split()
+def chunk_text(text: str) -> list[str]:
+    startingSentence = 0
+    wordsInChunk = 0
     chunks = []
-    for i in range(0, len(words), window_size - overlap):
-        chunks.append(' '.join(words[i:i + window_size]))
+    sentences = sent_tokenize(text)
+    for i in range(0,  len(sentences)):
+        words = sentences[i].split()
+        if wordsInChunk < CHUNK_SIZE:
+            wordsInChunk += len(words)
+        else:
+            wordsInChunk = 0
+            chunks.append(" ".join(sentences[startingSentence : i]))
+            startingSentence = i
+    chunks.append(" ".join(sentences[startingSentence:]))
+    #for chunk in chunks:
+    #    print(f"CHUNK: {chunk}")
+
     return chunks
 
-
 def generate_qa_pairs(text: str) -> dict[str, any]:
-    text_pairs = {}
     qa_pairs = []
 
     text = re.sub(r'[\t\n]', '', text)
-    chunks = chunk_text(text, 200, 0)
+    chunks = chunk_text(text)
     for i in range(len(chunks)):
-        phrases = sent_tokenize((rephrase(chunks[i])))
+        phrases = sent_tokenize(rephrase(chunks[i]))
         if i == len(chunks) - 1:
-            qa_pairs += generate_questions_monocontext(phrases, chunks[i-1] + chunks[i])
+            qa_pairs += generate_questions_monocontext(phrases, " ".join([chunks[i - 1], chunks[i]]))
         else:
-            qa_pairs += generate_questions_monocontext(phrases, chunks[i] + chunks[i+1])
+            qa_pairs += generate_questions_monocontext(phrases, " ".join([chunks[i], chunks[i+1]]))
 
-    text_pairs.update({"context": text, "qa_pairs": qa_pairs})
-
-    # Left here for testing without chunking
-
-    # phrases = sent_tokenize(rephrase(text))
-    # qa_pairs = generate_questions_monocontext(phrases, text)
-    # text_pairs = {"context": text, "qa_pairs": qa_pairs}
-    # text_pairs.update({"context": text, "qa_pairs": qa_pairs})
+    text_pairs = {"context": text, "qa_pairs": qa_pairs}
 
     return text_pairs
-
