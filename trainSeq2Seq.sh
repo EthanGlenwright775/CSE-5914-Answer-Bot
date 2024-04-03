9d57dd8ce992ab295118ecad5e68bd8aca1adc67
EthanGlenwright775 <112444137+EthanGlenwright775@users.noreply.github.com>
2024-04-03 09:38:07 -0400

    fix to storage

--- trainSeq2Seq.sh
+++ trainSeq2Seq.sh
@@ -1,20 +1,15 @@
-export HF_DATASETS_CACHE="/research/nfs_fosler_1/models/huggingface_cache"
 export CUDA_VISIBLE_DEVICES=1
 
-
 # Make sure LOGGER_NAME and --model_name are correct
-LOGGER_NAME="t5_xl_goldFacts"
+LOGGER_NAME="t5_answer_bot_v1"
 python3 lightning_t5_trainer.py \
-	--model_directory "/research/nfs_fosler_1/amadh/cosi/modelStore/" \
-	--model_path "/research/nfs_fosler_1/amadh/cosi/modelStore/${LOGGER_NAME}/" \
+	--model_directory "QA-Bot/" \
+	--model_path "QA-Bot/t5_answer_bot_v1/${LOGGER_NAME}/" \
 	--train_data "QA-Output/training.tsv" \
 	--dev_data "QA-Output/validation.tsv" \
 	--test_data "QA-Output/testing.tsv" \
-	--evidence_data "data/evidence.tsv" \
 	--num_turns 3 \
 	--model_name "google/flan-t5-xl" \
-	--generation_input ""data/llama_fine_tune_dev_set.json"" \
-	--generation_output "data/results/${LOGGER_NAME}_dev_generations.tsv" \
 	--data_seed 42 \
 	--train_seed 42 \
 	--dev_ratio 0.1 \
